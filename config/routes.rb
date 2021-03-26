54b084f90bf059cf71a590ebbd74897a416394cd
Zach Parkes <zach@sprigghr.com>
2021-03-26 12:39:34 -0400

    Add Read and Update features

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
 end
