bdb2158772fcacde6692a7c597c6f2251fe2c160
Zach Parkes <zach@sprigghr.com>
2021-03-29 10:46:43 -0400

    Add destroy functionality, layout links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
 end
